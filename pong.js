fbcb26a44e8304d80eb0a3e29c444a3caffef2a0
Mike-ops273 <mivaxd55@yahoo.com>
2020-12-21 17:01:08 +0200

    making my initial commit

---
+++ pong.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("pongCanvas");
+/** "document.getElementById()"" is a method that returns an "Element" object of the same name 
+ * entered into the paranthesis with quotes.
+ */
+const context= canvas.getContext("2d"); 
+/** "canvas.getContext()" will give you Methods and Properties to draw to the Canvas.  
+ * 
+*/
